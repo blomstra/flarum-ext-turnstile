9f5d5f2bba79ed1088f37c4078069d2d381821be
Sami Mazouz <sychocouldy@gmail.com>
2023-01-19 23:58:59 +0100

    fix: change password modal does not add turnstile token (#3)

---
+++ js/src/forum/extend/addTurnstileToChangePassword.tsx
@@ -0,0 +1,57 @@
+import app from 'flarum/forum/app';
+import { override } from 'flarum/common/extend';
+import Button from 'flarum/common/components/Button';
+import ChangePasswordModal from 'flarum/forum/components/ChangePasswordModal';
+
+import Turnstile from '../components/Turnstile';
+
+export default function addTurnstileToForgotPassword() {
+  ChangePasswordModal.prototype.__turnstileToken = null;
+
+  override(ChangePasswordModal.prototype, 'content', function (this: ChangePasswordModal, original) {
+    if (!!!app.forum.attribute('blomstra-turnstile.forgot')) return;
+
+    return (
+      <div className="Modal-body">
+        <div className="Form Form--centered">
+          <p className="helpText">{app.translator.trans('core.forum.change_password.text')}</p>
+          <div className="Form-group">
+            {Button.component(
+              {
+                className: 'Button Button--primary Button--block',
+                type: 'submit',
+                loading: this.loading,
+              },
+              app.translator.trans('core.forum.change_password.send_button')
+            )}
+          </div>
+          <Turnstile
+            action="forgot_pw"
+            onTurnstileStateChange={(token) => {
+              this.__turnstileToken = token;
+            }}
+          />
+        </div>
+      </div>
+    );
+  });
+
+  override(ChangePasswordModal.prototype, 'onsubmit', function (this: ChangePasswordModal, original, e: SubmitEvent) {
+    if (!!!app.forum.attribute('blomstra-turnstile.forgot')) return;
+
+    e.preventDefault();
+
+    this.loading = true;
+
+    app
+      .request({
+        method: 'POST',
+        url: app.forum.attribute('apiUrl') + '/forgot',
+        body: {
+          email: app.session.user!.email(),
+          turnstileToken: this.__turnstileToken,
+        },
+      })
+      .then(this.hide.bind(this), this.loaded.bind(this));
+  });
+}
