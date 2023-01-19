13eb494178e652a198df9103cb0bb5eb56e5aaea
Sami Mazouz <sychocouldy@gmail.com>
2023-01-19 23:02:25 +0100

    fix: change password modal does not add turnstile token

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,11 +1,12 @@
 import app from 'flarum/forum/app';
 import addTurnstileToForgotPassword from './extend/addTurnstileToForgotPassword';
+import addTurnstileToChangePassword from './extend/addTurnstileToChangePassword';
 import addTurnstileToLogin from './extend/addTurnstileToLogin';
-
 import addTurnstileToSignUp from './extend/addTurnstileToSignUp';
 
 app.initializers.add('blomstra/turnstile', () => {
   addTurnstileToSignUp();
   addTurnstileToLogin();
   addTurnstileToForgotPassword();
+  addTurnstileToChangePassword();
 });
